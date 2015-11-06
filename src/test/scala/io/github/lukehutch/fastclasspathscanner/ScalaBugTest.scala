008b2477159ddb98c6ec59d627144aaa96e68c46
Tin Pavlinic <tin@simplemachines.com.au>
2015-11-06 12:10:10 +1100

    Make test actually fail

--- src/test/scala/io/github/lukehutch/fastclasspathscanner/ScalaBugTest.scala
+++ src/test/scala/io/github/lukehutch/fastclasspathscanner/ScalaBugTest.scala
@@ -12,8 +12,8 @@ class ScalaBugTest {
   @Test
   def findObjectsTraitOtherPackage = {
     //This reroduces #29. The line below should work, but we have to add the outside trait's package to make it all compile
-//    val scanner = new FastClasspathScanner("io.github.lukehutch.fastclasspathscanner")
-    val scanner = new FastClasspathScanner("io.github.lukehutch.fastclasspathscanner", "someothersillypackage")
+    val scanner = new FastClasspathScanner("io.github.lukehutch.fastclasspathscanner")
+//    val scanner = new FastClasspathScanner("io.github.lukehutch.fastclasspathscanner", "someothersillypackage")
     assertFalse(
       scanner.verbose().scan()
       .getNamesOfClassesImplementing("someothersillypackage.Outside").isEmpty
